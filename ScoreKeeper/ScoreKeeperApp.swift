246deb8dc5e74e94cfd3d396b4a4bd26827ac3b9
adamcaak <ladamczak1997@gmail.com>
2025-07-04 23:08:55 +0200

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Łukasz Adamczak on 04/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
