34f97c0c9fa52ec427f6107a7765f13feb2983d7
adamcaak <ladamczak1997@gmail.com>
2025-07-05 23:29:21 +0200

    Create tests file

---
+++ ScoreKeeperTests/ScoreKeeperTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScoreKeeperTests.swift
+//  ScoreKeeperTests
+//
+//  Created by Łukasz Adamczak on 05/07/2025.
+//
+
+import Testing
+
+struct ScoreKeeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
