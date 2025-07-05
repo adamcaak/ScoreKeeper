f0231f85ad98574c9a75722b3ba9cb4f2c985eb6
adamcaak <ladamczak1997@gmail.com>
2025-07-06 00:15:13 +0200

    Create SettingsView

---
+++ ScoreKeeper/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  ScoreKeeper
+//
+//  Created by Łukasz Adamczak on 06/07/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
